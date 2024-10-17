f9db7911cf3f89221de1ede4f6082ab028868009
Ezio <133083000+DaniAuditore@users.noreply.github.com>
2024-10-17 12:36:05 -0300

    feat: add MyLilCard.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,36 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MylCard v-for="card in cards" :key="card.ide" :card="card" :edid="edid" />
+  <p>{{ card }}</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MylCard from './components/MylCard.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MylCard
+  },
+  data() {
+    return {
+      // Definimos la carta con ide 009
+      cards: [
+        {ide: '009' },{ide: '010'}
+      ],
+      edid: '125', // ID de la edición
+    };
+  },
 }
-
-//re organizar cosas dentro de carpetas
-
 </script>
 
 <style>
+
+.card-container {
+  display: flex; /* Permite que las cartas se alineen en una fila */
+  flex-wrap: wrap; /* Permite que las cartas se envuelvan si no hay espacio suficiente */
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

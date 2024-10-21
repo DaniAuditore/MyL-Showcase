ca1b8069dbf47aff6f854cf3149565f234f7a437
cris <c.cariqueo03@ufromail.cl>
2024-10-21 02:19:02 -0300

    fix(inventory): cards view fixed
    src/components/inventory/deck-view.vue
    + Cards join on groups if there are more than one of each
    + Added a label to show how much cards there are in a group
    + Each row alligns nicelly

--- src/components/inventory/deck-view.vue
+++ src/components/inventory/deck-view.vue
@@ -12,8 +12,13 @@
 
         <figure class="cards">
             <div class="cards-foreach" v-for="card in this.deck.cards" :key="card.ide">
-                <MylCard :card="card" :edid="card.edid"/>
-                <!-- <div class="card">{{ card }}</div> -->
+                <div v-for="index in card.count" :key="index">
+                    <MylCard class="card" :card="card" :edid="card.edid" 
+                        v-if="index < 4"
+                        :class="{ card_small: isRepeated(index, card.count) }" 
+                    />
+                    <label class="card_count" for="card" v-if="index === 1">{{ card.count }}</label>
+                </div>
             </div>
         </figure>
 </section>
@@ -51,6 +56,11 @@ export default {
             return length;
         }
     },
+    methods: {
+        async isRepeated(index, count) {
+            return await index < count;
+        }
+    }
 }
 </script>
 
@@ -98,15 +108,37 @@ export default {
     top: 70px;
     padding: 10px 0;
 
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    grid-template-rows: repeat(auto, 100px);
+
     overflow-y: scroll;
     scrollbar-width: none;
 
     border-top: 1px solid var(--primary-border-color);
 }
-.card {
-    width: 15%;
-    height: fit-content;
+.cards-foreach {
+    position: relative;
+    float: left;
+    
+    width: auto;
+    height: auto;
     
     margin: 0 5px;
+}
+.card_small {
+    width: auto;
+    height: 30px;
+}
+
+.card_count {
+    position: absolute;
+    top: 0;
+    left: 50%;
+
+    transform: translateX(-50%);
+    
+    color: var(--primary-background-color);
+    background-image: radial-gradient(circle, rgb(163 163 163 / 50%) 39%, rgb(255 255 255 / 0%) 96%);
 }
 </style>

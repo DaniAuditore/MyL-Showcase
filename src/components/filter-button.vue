c4503c9f573c802fb65a1253c49e23e07ec26ab7
cris <c.cariqueo03@ufromail.cl>
2024-10-21 18:23:30 -0300

    feat(inventory-filter): filter button implemented
    src/components/filter-button.vue
    + Created filter button component
    
    src/pages/inventory-page.vue
    + Filter button imported
    + Commented line on .content deleted

---
+++ src/components/filter-button.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+    <button class="filter-button">Filtro</button>
+</div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.filter-button {
+    width: 147px;
+    height: 85px;
+    position: absolute;
+    right: 0;
+    top: 0;
+
+    align-content: center;
+    text-align: center;
+    font-size: 200%;
+    font-weight: 100;
+
+    cursor: pointer;
+    
+    color: var(--primary-border-color);
+    background-color: var(--primary-background-color);
+    border-radius: 30px;
+    border: 2px solid var(--primary-border-color);
+}
+
+.filter-button:hover {
+    color: var(--primary-text-color);
+}
+
+.filter-button:active {
+    filter: brightness(125%);
+}
+</style>

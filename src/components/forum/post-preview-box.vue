d6787a9b7bacf8fd57ed93fb8127bb3afaf86806
cris <c.cariqueo03@ufromail.cl>
2024-10-22 00:51:11 -0300

    feat(forum): forum implemented
    src/router/index.js
    + Path to forum added
    
    src/pages/forum-page.vue
    + Component for forum page created
    
    src/components/forum/post-preview.vue
    + Post preview component created
    
    src/components/forum/post-preview-box.vue
    + Created component to show the post box and later link to post full view
    
    src/components/Side-bar.vue
    + Corrected spelling on isActive() for forum route

---
+++ src/components/forum/post-preview-box.vue
@@ -0,0 +1,21 @@
+<template>
+    <router-link :to="'/forum/'
+    //  + post.id
+    " class="post_box"></router-link>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.post_box {
+    width: 1103px;
+    height: 180px;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+
+    border-radius: 30px;
+    border: 2px solid var(--primary-border-color);
+}
+</style>
---
+++ src/components/forum/post-preview.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="post">
+
+
+    <postPreviewBox/>
+</div>    
+</template>
+
+<script setup>
+import postPreviewBox from './post-preview-box.vue';
+</script>
+
+<style scoped>
+.post {
+    width: 1107px;
+    height: 184px;
+    position: relative;
+    left: calc(50% - 551px);
+
+    margin-bottom: 10px;
+    background: var(--primary-background-color);
+    overflow: hidden;
+}
+</style>

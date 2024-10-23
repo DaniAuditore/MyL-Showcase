3bf4f267567c4e133bc6e3de22d740f8389c6091
Ezio <133083000+DaniAuditore@users.noreply.github.com>
2024-10-23 09:10:40 -0300

    fix: update styles in rules-page.vue to use scoped styles

---
+++ src/components/card-section.vue
@@ -0,0 +1,36 @@
+<template>
+    <section class="card-section">
+      <h2>{{ title }}</h2>
+      <div class="horizontal-images">
+        <myl-card v-for="card in cards" :key="card.ide" :card="card" :edid="card.edid" />
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      cards: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .card-section {
+    margin-bottom: 5%;
+  }
+  
+  .horizontal-images {
+    display: flex;
+    gap: 1rem;
+    overflow-x: scroll;
+  }
+  </style>
+  

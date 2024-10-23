cf747970703a25ca60f17c3f6397ed86e3a9e28f
cris <c.cariqueo03@ufromail.cl>
2024-10-23 05:40:39 -0300

    feat(collection): frist view of collection
    src/components/collection/collection-card.vue
    + Created component and defined looks
    
    src/pages/collection-page.vue
    + Created component and added sample cards in an array
    
    src/router/index.js
    + Added path to cllection-page

---
+++ src/components/collection/collection-card.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="collection_card">
+    <div class="frame">
+        <mylCard :card="card" :edid="edid"/>
+    </div>
+    <input type="checkbox" class="check" name="" id="">
+</div>
+</template>
+
+<script>
+import mylCard from '../myl-card.vue';
+
+export default {
+    components: {
+        mylCard
+    },
+    props: {
+        card: {
+            type: Object,
+            required: true
+        },
+        edid: {
+            type: String,
+            required: true
+        },
+    }
+};
+</script>
+
+<style scoped>
+.collection_card {
+    width: 100%;
+    height: 100%;
+
+    position: relative;
+    
+    overflow: visible;
+}
+
+.frame {
+    width: fit-content;
+    height: fit-content;
+
+    padding: 10px;
+    overflow: visible;
+
+    background-color: var(--secondary-background-color);
+    border-radius: 10px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+}
+
+.check {
+    width: 30px;
+    height: 30px;
+    position: absolute;
+    right: -40px;
+    top: 52px;
+}
+</style>

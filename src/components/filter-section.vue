8fcf08ebbef9101a54dfba75d2e3203ad64986c8
Ezio <133083000+DaniAuditore@users.noreply.github.com>
2024-11-27 13:39:52 -0300

    fix: resolve image loading issue and add filter-section.vue component

---
+++ src/components/filter-section.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="filter-section">
+    <label :for="label">{{ label }}:</label>
+    <select v-if="type === 'select'" :id="label" class="styled-button" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+      <option value="">--Seleccionar--</option>
+      <option v-for="option in options" :key="option" :value="option">{{ option }}</option>
+    </select>
+    <input v-else :id="label" type="text" :placeholder="label" class="styled-button" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: String,
+    modelValue: String,
+    options: {
+      type: Array,
+      default: () => [],
+    },
+    type: {
+      type: String,
+      default: 'select',
+    },
+  },
+};
+</script>
